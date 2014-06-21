6996d12565990e225c9320dad952981975412eaa
cohart <me@colin-hart.com>
2014-06-21 13:31:10 -0400

    added comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-CSV.parse(File.read('lib/assets/596-data.csv')) do |row|
+CSV.parse(File.read('lib/assets/596-data')) do |row|
   x = Lot.new(address: row[0], borough: row[1], zipcode: row[2], agency_owner: row[3], area_sq_ft: row[4], area_acres: row[5], is_vacant: row[6], latitude: row[7], longitude: row[8] )
   x.save
 end
