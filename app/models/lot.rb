e79e5e3f137b592bf3534472e092d9d2e4c94c8e
alexshook <alexandra.shook@gmail.com>
2014-06-21 13:26:47 -0400

    setting up associations:

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -1,4 +1,7 @@
 class Lot < ActiveRecord::Base
+  has_many :comments
+  has_many :users, through: :comments
+
   has_attached_file :photo1, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo1, :content_type => /\Aimage\/.*\Z/
 
